ab5740b58b8cc14e13dd27fcab91fb6b5a4b962f
RXS3323 <rageeth_cheriyathpadinjarathil@homedepot.com>
2017-10-29 19:58:00 -0400

    check in changes for home page

--- src/main/java/com/example/DefaultController.java
+++ src/main/java/com/example/DefaultController.java
@@ -2,21 +2,24 @@ package com.example;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("/")
 public class DefaultController {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public String defaultController(HttpServletRequest request){
-        return "Welcome to Test App";
+    public String defaultController(@RequestParam(value="name", required=false, defaultValue="") String name, Model model,HttpServletRequest httpRequest){
+        model.addAttribute("name", name);
+        return "index";
     }
     
     
